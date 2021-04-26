73905be395fcc3cb5e1203eedd4aeaaf44c15819
olive <olive@DESKTOP-KQUM7A1>
2021-04-26 13:26:08 -0700

    rffffff

--- src/_05_vault/vault.java
+++ src/_05_vault/vault.java
@@ -6,15 +6,15 @@ import javax.swing.JOptionPane;
 
 public class vault {
 	
-static void tryCode() {
-	int code = 118;
-	for (int i = 0; i < 1001; i++) {
-		System.out.println(i);
-		if(i == code) {
-			JOptionPane.showMessageDialog(null, "The code was " + code + "!");
-			
-		}
-	}
+	int code = 9;
+	public vault(int waawawa){
+		
+			code = waawawa;
+}
+
+boolean tryCode(int in) {
+	
+	return code == in;
 	
 	
 }
--- src/_05_vault/vaultRunner.java
+++ src/_05_vault/vaultRunner.java
@@ -2,7 +2,9 @@ package _05_vault;
 
 public class vaultRunner {
 public static void main(String[] args) {
-	
+	 vault v = new vault(37471);
+	 jamesBond j = new jamesBond();
+	 System.out.println(j.findCode(v));
 }
 
 }
